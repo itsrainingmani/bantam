46885088997c38799a187025de07ff3ed38fed94
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-12-17 21:44:21 -0500

    implement all expressions and parselets

--- src/expression.rs
+++ src/expression.rs
@@ -12,6 +12,38 @@ pub struct NameExpression {
     name: String,
 }
 
+pub struct PrefixExpression {
+    operator: TokenType,
+    right: Box<dyn Expression>,
+}
+
+pub struct OperatorExpression {
+    left: Box<dyn Expression>,
+    operator: TokenType,
+    right: Box<dyn Expression>,
+}
+
+pub struct PostfixExpression {
+    left: Box<dyn Expression>,
+    operator: TokenType,
+}
+
+pub struct ConditionalExpression {
+    condition: Box<dyn Expression>,
+    then_arm: Box<dyn Expression>,
+    else_arm: Box<dyn Expression>,
+}
+
+pub struct AssignExpression {
+    name: String,
+    right: Box<dyn Expression>,
+}
+
+pub struct CallExpression {
+    function: Box<dyn Expression>,
+    args: Vec<Box<dyn Expression>>,
+}
+
 impl NameExpression {
     pub fn new(name: String) -> Self {
         Self { name }
@@ -32,11 +64,6 @@ impl Expression for NameExpression {
     }
 }
 
-pub struct PrefixExpression {
-    operator: TokenType,
-    right: Box<dyn Expression>,
-}
-
 impl PrefixExpression {
     pub fn new(tt: TokenType, right: Box<dyn Expression>) -> Self {
         Self {
@@ -51,6 +78,127 @@ impl Expression for PrefixExpression {
         builder.push_str("(");
         builder.push(self.operator.punctuator().unwrap());
         self.right.print(builder);
+        builder.push_str(")");
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl OperatorExpression {
+    pub fn new(left: Box<dyn Expression>, operator: TokenType, right: Box<dyn Expression>) -> Self {
+        Self {
+            left,
+            operator,
+            right,
+        }
+    }
+}
+
+impl Expression for OperatorExpression {
+    fn print(&self, builder: &mut String) -> () {
+        builder.push_str("(");
+        self.left.print(builder);
+        builder.push_str(" ");
+        builder.push(self.operator.punctuator().unwrap());
+        builder.push_str(" ");
+        self.right.print(builder);
+        builder.push_str(")");
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl PostfixExpression {
+    pub fn new(left: Box<dyn Expression>, operator: TokenType) -> Self {
+        Self { left, operator }
+    }
+}
+
+impl Expression for PostfixExpression {
+    fn print(&self, builder: &mut String) -> () {
+        builder.push_str("(");
+        self.left.print(builder);
+        builder.push(self.operator.punctuator().unwrap());
+        builder.push_str(")");
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl ConditionalExpression {
+    pub fn new(
+        condition: Box<dyn Expression>,
+        then_arm: Box<dyn Expression>,
+        else_arm: Box<dyn Expression>,
+    ) -> Self {
+        Self {
+            condition,
+            then_arm,
+            else_arm,
+        }
+    }
+}
+
+impl Expression for ConditionalExpression {
+    fn print(&self, builder: &mut String) -> () {
+        builder.push_str("(");
+        self.condition.print(builder);
+        builder.push_str(" ? ");
+        self.then_arm.print(builder);
+        builder.push_str(" : ");
+        self.else_arm.print(builder);
+        builder.push_str(")");
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl AssignExpression {
+    pub fn new(name: String, right: Box<dyn Expression>) -> Self {
+        Self { name, right }
+    }
+}
+
+impl Expression for AssignExpression {
+    fn print(&self, builder: &mut String) -> () {
+        builder.push_str("(");
+        builder.push_str(&self.name);
+        builder.push_str(" = ");
+        self.right.print(builder);
+        builder.push_str(")");
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl CallExpression {
+    pub fn new(function: Box<dyn Expression>, args: Vec<Box<dyn Expression>>) -> Self {
+        Self { function, args }
+    }
+}
+
+impl Expression for CallExpression {
+    fn print(&self, builder: &mut String) -> () {
+        self.function.print(builder);
+        builder.push_str("(");
+        let mut i = 0;
+        for arg in self.args.iter() {
+            arg.print(builder);
+            if i + 1 < self.args.len() {
+                builder.push_str(", ");
+            }
+            i += 1;
+        }
         builder.push_str(")");
     }
 
--- src/parselet.rs
+++ src/parselet.rs
@@ -1,5 +1,8 @@
-use crate::core::{Parser, Token};
-use crate::expression::{Expression, NameExpression, PrefixExpression};
+use crate::core::{Parser, Precedence, Token, TokenType};
+use crate::expression::{
+    AssignExpression, CallExpression, ConditionalExpression, Expression, NameExpression,
+    OperatorExpression, PostfixExpression, PrefixExpression,
+};
 
 // One of the two interfaces used by the Pratt parser. A PrefixParselet is
 // associated with a token that appears at the beginning of an expression. Its
@@ -15,6 +18,14 @@ pub trait PrefixParselet {
 // Bantam has one single-token expression: named variables
 pub struct NameParselet {}
 
+// We can use a single struct for all the prefix operators since they only differ in the actual operator token itself
+pub struct PrefixOperatorParselet {
+    precedence: Precedence,
+}
+
+// Parses Parentheses used to group an expression `a * (b + c)`
+pub struct GroupParselet {}
+
 impl NameParselet {
     pub fn new() -> Self {
         Self {}
@@ -28,21 +39,30 @@ impl PrefixParselet for NameParselet {
     }
 }
 
-// We can use a single struct for all the prefix operators since they only differ in the actual operator token itself
-pub struct PrefixOperatorParselet {
-    precedence: usize,
-}
-
 impl PrefixOperatorParselet {
-    pub fn new(precedence: usize) -> Self {
+    pub fn new(precedence: Precedence) -> Self {
         Self { precedence }
     }
 }
 
 impl PrefixParselet for PrefixOperatorParselet {
     fn parse(&self, parser: &mut Parser, token: Token) -> Box<dyn Expression> {
-        let operand = parser.parse_expression_precedence(self.precedence);
+        let operand = parser.parse_expression_precedence(self.precedence.clone());
         return Box::new(PrefixExpression::new(*token.get_type(), operand));
+    }
+}
+
+impl GroupParselet {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl PrefixParselet for GroupParselet {
+    fn parse(&self, parser: &mut Parser, _token: Token) -> Box<dyn Expression> {
+        let expr = parser.parse_expression();
+        parser.consume_expected(TokenType::RightParen);
+        expr
     }
 }
 
@@ -59,5 +79,164 @@ pub trait InfixParselet {
         left: Box<dyn Expression>,
         token: Token,
     ) -> Box<dyn Expression>;
-    fn get_precedence(&self) -> usize;
+    fn get_precedence(&self) -> Precedence;
+}
+
+pub struct BinaryOperatorParselet {
+    precedence: Precedence,
+    is_right: bool,
+}
+
+pub struct PostfixOperatorParselet {
+    precedence: Precedence,
+}
+
+// a ? b : c
+pub struct ConditionalParselet {}
+
+// a = b
+// left side has to be a name
+// expressions are right-associative
+// a = b = c becomes a = (b = c)
+pub struct AssignParselet {}
+
+pub struct CallParselet {}
+
+impl BinaryOperatorParselet {
+    pub fn new(precedence: Precedence, is_right: bool) -> Self {
+        Self {
+            precedence,
+            is_right,
+        }
+    }
+}
+
+impl InfixParselet for BinaryOperatorParselet {
+    fn parse(
+        &self,
+        parser: &mut Parser,
+        left: Box<dyn Expression>,
+        token: Token,
+    ) -> Box<dyn Expression> {
+        let op_prec = self.precedence as usize - if self.is_right { 1 } else { 0 };
+        let right = parser.parse_expression_precedence(op_prec.into());
+
+        Box::new(OperatorExpression::new(left, *token.get_type(), right))
+    }
+
+    fn get_precedence(&self) -> Precedence {
+        self.precedence
+    }
+}
+
+impl PostfixOperatorParselet {
+    pub fn new(precedence: Precedence) -> Self {
+        Self { precedence }
+    }
+}
+
+impl InfixParselet for PostfixOperatorParselet {
+    fn parse(
+        &self,
+        _parser: &mut Parser,
+        left: Box<dyn Expression>,
+        token: Token,
+    ) -> Box<dyn Expression> {
+        Box::new(PostfixExpression::new(left, *token.get_type()))
+    }
+
+    fn get_precedence(&self) -> Precedence {
+        self.precedence
+    }
+}
+
+impl ConditionalParselet {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl InfixParselet for ConditionalParselet {
+    fn parse(
+        &self,
+        parser: &mut Parser,
+        left: Box<dyn Expression>,
+        _token: Token,
+    ) -> Box<dyn Expression> {
+        let then_arm = parser.parse_expression();
+        parser.consume_expected(TokenType::Colon);
+
+        let else_prec = Precedence::Conditional as usize - 1;
+        let else_arm = parser.parse_expression_precedence(else_prec.into());
+        Box::new(ConditionalExpression::new(left, then_arm, else_arm))
+    }
+
+    fn get_precedence(&self) -> Precedence {
+        Precedence::Conditional
+    }
+}
+
+impl AssignParselet {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl InfixParselet for AssignParselet {
+    fn parse(
+        &self,
+        parser: &mut Parser,
+        left: Box<dyn Expression>,
+        _token: Token,
+    ) -> Box<dyn Expression> {
+        let right_prec = Precedence::Assignment as usize - 1;
+        let right = parser.parse_expression_precedence(right_prec.into());
+
+        let left_name_expr = match left.as_any().downcast_ref::<NameExpression>() {
+            Some(ne) => ne,
+            None => panic!("left hand side of assignment must be a name"),
+        };
+
+        let name = left_name_expr.name();
+        Box::new(AssignExpression::new(name.clone(), right))
+    }
+
+    fn get_precedence(&self) -> Precedence {
+        Precedence::Assignment
+    }
+}
+
+impl CallParselet {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl InfixParselet for CallParselet {
+    fn parse(
+        &self,
+        parser: &mut Parser,
+        left: Box<dyn Expression>,
+        _token: Token,
+    ) -> Box<dyn Expression> {
+        let mut args = Vec::new();
+
+        // Could be no args
+        if !parser.match_tok(TokenType::RightParen) {
+            loop {
+                args.push(parser.parse_expression());
+
+                if !parser.match_tok(TokenType::Comma) {
+                    break;
+                }
+            }
+            parser.consume_expected(TokenType::RightParen);
+        }
+
+        Box::new(CallExpression::new(left, args))
+    }
+
+    fn get_precedence(&self) -> Precedence {
+        Precedence::Call
+    }
 }
